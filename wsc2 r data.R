198e4677fdf135c37c0e62b54121ae3b58b93edf
StephenQueen7753 <144402865+StephenQueen7753@users.noreply.github.com>
2023-09-08 11:48:38 +0930

    Add files via upload

---
+++ wsc2 r data.R
@@ -0,0 +1,41 @@
+wheatdata <- read.csv("WSC2.csv")
+
+times <- wheatdata$Time
+
+
+# Time vs DW.AK -----------------------------------------------------------
+
+# create an object for each variable
+
+times <- wheatdata$Time
+dwak <-wheatdata$DW.AK
+
+# check distribution
+
+hist(dwak)
+shapiro.test(dwak)
+
+
+# anova
+dwak.anova <- aov(dwak~times, na.rm = TRUE)
+summary(dwak.anova)
+
+
+
+# Time vs DW.AS -----------------------------------------------------------
+
+
+# create an object for each variable
+
+times <- wheatdata$Time
+dwak <-wheatdata$DW.AS
+
+# check distribution
+
+
+hist(dwak)
+shapiro.test(dwak)
+
+
+
+
